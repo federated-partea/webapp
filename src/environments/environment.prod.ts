1eb79768086086fe1ce098cbd481aa011ccaa438
Julian Späth <Spaethju@posteo.de>
2023-07-17 16:22:01 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   environmentName: 'prod',
-  apiUrl: 'https://partea-api.zbh.uni-hamburg.de/',
-  cookieDomain: '*.zbh.uni-hamburg.de',
+  apiUrl: 'https://apps.cosy.bio/api-partea/',
+  cookieDomain: 'https://apps.cosy.bio/partea/',
 };
 
